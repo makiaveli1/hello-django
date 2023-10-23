b44cc4d7e216c964b69669ec390776942f585b34
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-23 10:55:38 +0100

    removed secret key and made it more secure

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,8 +16,11 @@ import dj_database_url
 
 development = os.environ.get('DEVELOPMENT', True)
 
-if os.path.isfile('todo/env.py'):
-   import env
+# settings.py
+if os.environ.get('SECRET_KEY'):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','')
+SECRET_KEY =('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
