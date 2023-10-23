d7a1b4bcd29fe7cd47d9710eff0c960efbf109b5
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-23 11:09:19 +0100

    fix issue with hostname second attempt

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -18,7 +18,9 @@ development = os.environ.get('DEVELOPMENT', True)
 
 # settings.py
 if os.environ.get('SECRET_KEY'):
-    SECRET_KEY = os.environ.get('SECRET_KEY',)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+else:
+    SECRET_KEY =('SECRET_KEY','')
 
 
 
@@ -30,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =('SECRET_KEY','')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEVELOPMENT'):
