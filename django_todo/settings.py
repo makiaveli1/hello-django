c12b2f9c2b0a59c6a59496c46b3d1a5b0d04e4a4
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-23 11:05:58 +0100

    fix issue with hostname

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -35,6 +35,8 @@ SECRET_KEY =('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEVELOPMENT'):
     DEBUG = development
+else:
+    DEBUG = False
     
 
 
